bbf47227ce2fac7e6c1b76fa96014d5b8ebd7559
Just <xiaofei0127@outlook.com>
2017-05-16 21:44:33 +0800

    Initial Commit

---
+++ RunTimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunTimeDemo
+//
+//  Created by PF on 2017/5/16.
+//  Copyright © 2017年 PF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
